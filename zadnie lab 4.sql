3e7f7a83ea9adc526da607d1999f28aaca40268a
Toxer12 <115220195+Toxer12@users.noreply.github.com>
2022-11-18 13:00:16 +0100

    praca na lekcji
    Lab 4

---
+++ zadnie lab 4.sql
@@ -0,0 +1,56 @@
+select * from postac;
+# zadanie 1
+# Ctrl +/ dodanie kometarz
+-- komentarz...
+/* komnterz */
+# Ctrl + d duplikacja lini
+
+# lab 4 zad 1a
+select * from postac 
+where nazwa != 'Bjorn'
+and rodzaj='wiking'
+order by data_ur ASC
+limit 2;
+delete from postac 
+where id_postaci=4;
+delete from postac 
+where id_postaci=5;
+
+# lab 4 zad 1b
+alter table postac drop primary key;
+# pornlem 1 - istnieja klucze obce
+# tabela walizka i statek
+#usuwanie kluczy obcych
+#alter table walizka drop foreign key
+#walizka_ibfk_1;
+#aby sprawdzic nazwy kluczy obcyh
+show create table walizka;
+alter table walizka drop foreign key
+walizka_ibfk_1;
+#problem 2 atrybut auto increment
+alter table postac modify id_postaci int;
+#drop primary key dziala
+
+# lab zad 2a
+# first wstawiamy kolumne jako pierwsza w tabeli
+alter table postac add pesel char(11) first;
+update postac set pesel=0;
+alter table postac add primary key(pesel);
+update postac set pesel='72819283718' + id_postaci;
+select * from postac;
+
+#lab4 zad 2b
+alter table postac modify rodzaj enum('wiking','ptak','kobieta','syrena');
+insert into postac values ('12345678901','7','Gertruda NIesczera','syrena','0932-03-09','764',default,default);
+
+#lab 4 zadanie 3a
+update postac set statek='wodnik'
+where nazwa like '%a%';
+select * from statek;
+
+#lab 4 zad 3b
+update statek set max_ladownosc= max_ladownosc*0.7
+where data_wodowania >= '0001-01-01'
+and data_wodowania <= '0100-12-31';
+#albo
+where year(data_wodowania) between 0001 and 0100;
